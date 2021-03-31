e31cb5d939e638f3a71dee0db97fcc2b626a249e
Alice Gao <alice.gao@raventelemetry.com>
2021-03-31 00:39:26 -0400

    Updates

--- js/init.js
+++ js/init.js
@@ -7,7 +7,6 @@
  jQuery(document).ready(function($) {
 
 
-
 /*----------------------------------------------------*/
 /* Smooth Scrolling
 ------------------------------------------------------ */
@@ -31,22 +30,19 @@
 ------------------------------------------------------*/
 
 	var sections = $("section");
-	var navigation_links = $("#nav-wrap a");
+	var navigation_links = $("#nav-wrap ul");
 
 	sections.waypoint({
-
       handler: function(event, direction) {
-
 		   var active_section;
 
-			active_section = $(this);
+         active_section = $(this);
 			if (direction === "up") active_section = active_section.prev();
 
-			var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
+         var active_link = $('#nav-wrap ul li a[href="#' + active_section.attr("id") + '"]');
 
          navigation_links.parent().removeClass("current");
 			active_link.parent().addClass("current");
-
 		},
 		offset: '35%'
 
